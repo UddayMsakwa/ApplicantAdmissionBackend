a6e5d77169423bc4359d8c815a180c23f6aa626d
Udday Msakwa <uddaymsakwa@gmail.com>
2025-11-22 20:20:31 +0700

    Add initial fluent API configurations for core entities
    Includes:
    - ApplicantConfiguration
    - ManagerConfiguration
    - EducationLevelConfiguration
    - EducationDocumentTypeConfiguration
    
    Sets up keys, required fields, and basic relationships for the core domain.

---
+++ ApplicantAdmission.DataAccess/Configurations/ApplicantConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ApplicantAdmission.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApplicantAdmission.DataAccess.Configurations;
+
+public class ApplicantConfiguration : IEntityTypeConfiguration<Applicant>
+{
+    public void Configure(EntityTypeBuilder<Applicant> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Email).IsRequired();
+        builder.Property(x => x.PasswordHash).IsRequired();
+        builder.Property(x => x.FullName).IsRequired();
+        builder.Property(x => x.Citizenship).IsRequired();
+        builder.Property(x => x.Gender).IsRequired();
+        builder.Property(x => x.Phone).IsRequired();
+
+        builder.HasMany(x => x.Documents)
+            .WithOne(x => x.Applicant)
+            .HasForeignKey(x => x.ApplicantId);
+
+        builder.HasMany(x => x.Admissions)
+            .WithOne(x => x.Applicant)
+            .HasForeignKey(x => x.ApplicantId);
+    }
+}
---
+++ ApplicantAdmission.DataAccess/Configurations/EducationLevelConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ApplicantAdmission.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApplicantAdmission.DataAccess.Configurations;
+
+public class EducationLevelConfiguration : IEntityTypeConfiguration<EducationLevel>
+{
+    public void Configure(EntityTypeBuilder<EducationLevel> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name).IsRequired();
+
+        builder.HasMany(x => x.DocumentTypes)
+            .WithOne(x => x.Level)
+            .HasForeignKey(x => x.LevelId);
+    }
+}
