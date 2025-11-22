726f5317e2264b3a46d39df6dd49209fd902f093
Udday Msakwa <uddaymsakwa@gmail.com>
2025-11-22 20:50:44 +0700

    Add fluent configurations for admission workflow and file metadata
    Includes:
    - AdmissionProgramConfiguration mapping programs to admission entries
    - ApplicantAdmissionConfiguration defining status, timestamps, and FK relations
    - FileEntityConfiguration with metadata rules and document relationships
    
    Advances the domain configuration for admissions and file handling.

---
+++ ApplicantAdmission.DataAccess/Configurations/AdmissionProgramConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ApplicantAdmission.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApplicantAdmission.DataAccess.Configurations;
+
+public class AdmissionProgramConfiguration : IEntityTypeConfiguration<AdmissionProgram>
+{
+    public void Configure(EntityTypeBuilder<AdmissionProgram> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasOne(x => x.Program)
+            .WithMany(x => x.AdmissionPrograms)
+            .HasForeignKey(x => x.ProgramId);
+    }
+}
---
+++ ApplicantAdmission.DataAccess/Configurations/ApplicantAdmissionConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using ApplicantAdmission.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApplicantAdmission.DataAccess.Configurations;
+
+public class ApplicantAdmissionConfiguration : IEntityTypeConfiguration<ApplicantAdmission>
+{
+    public void Configure(EntityTypeBuilder<ApplicantAdmission> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Status)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+
+        builder.HasOne(x => x.Applicant)
+            .WithMany(x => x.Admissions)
+            .HasForeignKey(x => x.ApplicantId);
+
+        builder.HasOne(x => x.AdmissionProgram)
+            .WithMany(x => x.ApplicantAdmissions)
+            .HasForeignKey(x => x.AdmissionProgramId);
+
+        builder.HasOne(x => x.Manager)
+            .WithMany(x => x.Admissions)
+            .HasForeignKey(x => x.ManagerId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
