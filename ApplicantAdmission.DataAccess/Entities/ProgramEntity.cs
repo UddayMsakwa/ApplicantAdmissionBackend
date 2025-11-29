c5e242043f029008247909af69d0470f1c4b69d7
Udday Msakwa <uddaymsakwa@gmail.com>
2025-11-29 16:14:27 +0700

    Restore ProgramEntity after accidental clean

---
+++ ApplicantAdmission.DataAccess/Entities/ProgramEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApplicantAdmission.DataAccess.Entities;
+
+public class ProgramEntity
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    public Guid FacultyId { get; set; }
+    public Guid LevelId { get; set; }
+
+    public Faculty Faculty { get; set; } = null!;
+    public EducationLevel Level { get; set; } = null!;
+
+    public ICollection<AdmissionProgram> AdmissionPrograms { get; set; }
+        = new List<AdmissionProgram>();
+}
