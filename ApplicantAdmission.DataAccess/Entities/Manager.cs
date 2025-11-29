f4a1e60992b01fede81a7e5637db77c7608d1d87
Udday Msakwa <uddaymsakwa@gmail.com>
2025-11-29 20:15:00 +0700

    Fix Manager entity, ApplicantAdmissionEntity consistency, build restored

--- ApplicantAdmission.DataAccess/Entities/Manager.cs
+++ ApplicantAdmission.DataAccess/Entities/Manager.cs
@@ -5,10 +5,8 @@ public class Manager
     public Guid Id { get; set; }
 
     public string Email { get; set; } = null!;
-    public string PasswordHash { get; set; } = null!;
     public string FullName { get; set; } = null!;
 
-    public string Role { get; set; } = null!; // Manager, HeadManager, Admin
 
     public ICollection<ApplicantAdmission> Admissions { get; set; } = new List<ApplicantAdmission>();
 }
