71df2805914d298bdf4fb40e16f7012303c2ced6
Udday Msakwa <uddaymsakwa@gmail.com>
2025-12-12 01:23:45 +0700

    feat: implement admissions workflow, documents API and DB seeding

--- ApplicantAdmission.DataAccess/ApplicantDbContext.cs
+++ ApplicantAdmission.DataAccess/ApplicantDbContext.cs
@@ -10,7 +10,7 @@ public class ApplicantDbContext : DbContext
     {
     }
 
-    // --- DbSets ---
+    
     public DbSet<Applicant> Applicants => Set<Applicant>();
     public DbSet<Manager> Managers => Set<Manager>();
     public DbSet<Document> Documents => Set<Document>();
@@ -33,47 +33,56 @@ public class ApplicantDbContext : DbContext
         
         modelBuilder.Entity<Document>().UseTptMappingStrategy();
 
-        // Seed data for my intial phase testing of application adna dmission creation.
-
+        
         var facultyId = Guid.Parse("11111111-1111-1111-1111-111111111111");
         var levelId = Guid.Parse("22222222-2222-2222-2222-222222222222");
         var programId = Guid.Parse("33333333-3333-3333-3333-333333333333");
         var admissionProgramId = Guid.Parse("44444444-4444-4444-4444-444444444444");
         var managerId = Guid.Parse("55555555-5555-5555-5555-555555555555");
 
-        modelBuilder.Entity<Faculty>().HasData(new Faculty
-        {
-            Id = facultyId,
-            Name = "Engineering"
-        });
+        modelBuilder.Entity<Faculty>().HasData(
+            new Faculty
+            {
+                Id = facultyId,
+                Name = "Engineering"
+            }
+        );
 
-        modelBuilder.Entity<EducationLevel>().HasData(new EducationLevel
-        {
-            Id = levelId,
-            Name = "Bachelor"
-        });
+        modelBuilder.Entity<EducationLevel>().HasData(
+            new EducationLevel
+            {
+                Id = levelId,
+                Name = "Bachelor"
+            }
+        );
 
-        modelBuilder.Entity<ProgramEntity>().HasData(new ProgramEntity
-        {
-            Id = programId,
-            Name = "Computer Science",
-            FacultyId = facultyId,
-            LevelId = levelId
-        });
+        modelBuilder.Entity<ProgramEntity>().HasData(
+            new ProgramEntity
+            {
+                Id = programId,
+                Name = "Computer Science",
+                FacultyId = facultyId,
+                LevelId = levelId
+            }
+        );
 
-        modelBuilder.Entity<AdmissionProgram>().HasData(new AdmissionProgram
-        {
-            Id = admissionProgramId,
-            ProgramId = programId
-        });
+        modelBuilder.Entity<AdmissionProgram>().HasData(
+            new AdmissionProgram
+            {
+                Id = admissionProgramId,
+                ProgramId = programId
+            }
+        );
 
-        modelBuilder.Entity<Manager>().HasData(new Manager
-        {
-            Id = managerId,
-            FullName = "Admin Manager",
-            Email = "manager@test.com",
-            PasswordHash = "hashed_pass",
-            Role = "Admin"
-        });
+        modelBuilder.Entity<Manager>().HasData(
+            new Manager
+            {
+                Id = managerId,
+                FullName = "Admin Manager",
+                Email = "manager@test.com",
+                PasswordHash = "hashed_pass",
+                Role = "Admin"
+            }
+        );
     }
 }
---
+++ ApplicantAdmission.DataAccess/Migrations/20251210180610_FixDbContextCleanUp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicantAdmission.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDbContextCleanUp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
